a556f27e1de59a94e6083888b95cf65863152f90
duy <duy211099@gmail.com>
2023-06-13 10:35:22 +0700

    updare programid

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -2,7 +2,7 @@
 use anchor_lang::prelude::*;
 
 // Basically, this is the "program id" and has info for Solana on how to run our program. Anchor has generated this one for us. We'll be changing it later.
-declare_id!("EjoBwG3XANnFLkFMnEF8EAKSXWKB92Xdszfujk3g2CP6");
+declare_id!("8DZwacAC3d58iEFZBhoxzpnF6Bt8C1opNBnQesdRCjPj");
 
 // This is how we tell our program, "Hey — everything in this little module below is our program that we want to create handlers for that other people can call". 
 // macros 
